a74b173327e7405941cd155c1d9edfb2258b2511
Umar Ashraf <hurricanebuz@gmail.com>
2023-07-11 16:26:08 +0500

    Changed name Hello.js to HellePeeps.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Hello from './Hello';
-import NotFound from './NotFound';
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./HelloPeeps";
+import NotFound from "./NotFound";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-      <Hello />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React JS
-        </a>
-      </header>
-    </div>
-    <NotFound />
+      <div className="App">
+        <header className="App-header">
+          <Hello />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React JS
+          </a>
+        </header>
+      </div>
+      <NotFound />
     </>
   );
 }
